2beff918044e1d39cd934459558e41083f9a434d
saopigqwq233 <904139266@qq.com>
2024-03-09 12:18:50 +0800

    归并排序

--- re_learn_ds/MergeSort.cpp
+++ re_learn_ds/MergeSort.cpp
@@ -1,43 +1,69 @@
 #include "iostream"
-#include "vector"
+
 using namespace std;
 
-int* merge(int* aB,int* aE,int* bB,int* bE){
-    int *tmp = new int[aE-aB+(aE-aB)+2];
-    int *i = tmp;
-    while (aB!=aE&&bB!=bE){
-        if(*aB<*bB)
-            *i = *aB;
-        else
-            *i = *bB;
-        i++;
-    }
-    for(;aB!=(aE+1);aB++,i++) *i = *aB;
-    for(;bB!=(bE+1);bB++,i++) *i = *bB;
-    return tmp;
-}
-void merge_sort(int *a,int b_index,int e_index){
+void merge(int*a,int low,int mid,int high){
+    int *b = new int [high-low+1];
+    int i=low,j=mid+1,k=0;
+    while (i<=mid&&j<=high){
+        if(a[i]<a[j]){
+            b[k++] = a[i++];
+        } else
+            b[k++] = a[j++];
 
+    }
+    while (i<=mid)
+        b[k++] = a[i++];
+    while (j<=high)
+        b[k++] = a[j++];
+    k=0;
+    for (int l = low; l <=high; ++l) {
+        a[l] = b[k++];
+    }
+    delete []b;
 }
-void my_mergesort(int *a,int n){
-    int left = 0,right = n-1;
-    merge_sort(a,left,right);
+void merge_sort(int*a,int low,int high){
+    if(low==high)
+        return;
+    else {
+        int mid = (low+high)/2;
+        merge_sort(a, low, mid);
+        merge_sort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+}
+void merge_sort(int a[],int n){
+    int i,s;
+    int t =1;
+    while (t<n){
+        s = t;
+        t*=2;
+        i=0;
+        while (i+t<n){
+            merge(a,i,i+s-1,i+s*2-1);
+        }
+        if(i+s<n){
+            merge(a,i,i+s-1,n-1);
+        }
+    }
 }
 int main(){
-    int a[10];
-    srand(10);
-    for (int i = 0; i < 10; ++i) {
-        a[i] = rand()%100;
+    int n=10;
+    srand(9);
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand()%100;
     }
-    cout<<"1"<<endl;
-    my_mergesort(a,10);
-    for (int i = 0; i < 10; ++i) {
-        cout<<a[i]<<' ';
+    for (int i = 0; i < n; ++i) {
+        cout<<arr[i]<<' ';
     }
+    cout<<endl;
+    merge_sort(arr,n);
+    for (int i = 0; i < n; ++i) {
+        cout<<arr[i]<<' ';
+    }
+    cout<<endl;
 }
-
-
-
 
 
 
